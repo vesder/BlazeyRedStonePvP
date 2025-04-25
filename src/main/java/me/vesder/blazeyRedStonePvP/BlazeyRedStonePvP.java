ff557f83800270cd0b4c3b99fc9e175e8ae3069d
vesder <vsdrrt124@gmail.com>
2025-04-25 14:36:29 +0330

    initialize the repo

---
+++ src/main/java/me/vesder/blazeyRedStonePvP/BlazeyRedStonePvP.java
@@ -0,0 +1,13 @@
+package me.vesder.blazeyRedStonePvP;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BlazeyRedStonePvP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+}
